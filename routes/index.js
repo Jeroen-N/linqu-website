250d07b2825f2a50c3e4eaff112bbcce49839531
Albert Pool <albertpool@solcon.nl>
2017-10-07 17:09:38 +0200

    Process contact form requests with Nodemailer

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,32 @@ router.get('/', (req, res, next) => {
     title: "LinqU - Don't let your place of residence limit your career!"
   });
 });
+router.post('/', (req, res, next) => {
+  var nodemailer = require('nodemailer');
+  var transporter = nodemailer.createTransport({
+    host: 'SMTP SERVER',
+    port: 587,
+    secure: false, /* Use STARTTLS, instead of deprecated SMTPS */
+    auth: {
+      user: 'SMTP USER',
+      pass: 'SMTP PASSWORD'
+    }
+  });
+  var mailOptions = {
+    from: 'info@linqu.io',
+    to: 'info@linqu.io',
+    replyTo: req.body.email,
+    subject: 'Contactformulier: ' + req.body.subject,
+    text: req.body.message
+  };
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+      res.send('An error has occured');
+    } else {
+      res.send('OK');
+    }
+  }); 
+});
 
 export default router;
